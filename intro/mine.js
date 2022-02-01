6a87ba24362d7ddd92cb9581b95357f59f34ad5b
igorots <3608748@gmail.com>
2022-02-01 16:03:38 +0200

    Initial commit

---
+++ intro/mine.js
@@ -0,0 +1,14 @@
+//1 Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let messag = 'hello'
+let wat = 'owu'
+let domain = 'com'
+let country = 'ua'
+let $ = 1
+let _ = 10
+let sally = -999 ;
+let ttt = 123
+let pi = 3.14
+let hot = 16
+var question = true
+var answer = false
+//2 Вивести кожну змінну за допомогою: console.log , alert, document.write
