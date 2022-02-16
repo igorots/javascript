79695003b3076fb247abb32dc88c7e58326a1488
igorots <3608748@gmail.com>
2022-02-16 12:41:12 +0200

    закінчив 2хомворк

--- lesson2/js/homework.js
+++ lesson2/js/homework.js
@@ -24,33 +24,66 @@ if (namber <=10){
 }*/
 // - У нас є змінна test, якщо дорівнює true, виведіть 'Вірно', інакше виведіть 'Неправильно'.
 // Перевірте роботу скрипта при test, що дорівнює true, false. Напишіть два варіанти скрипта - з коротким записом(тернаркою)
-let  test = !!100 ? 1 : 2;
-console.log(test)
+
+
+/*let test = !!100 ? console.log('ВІРНО') : console.log('НЕВІРНО');
+console.log(test)*/
+//-----------------------------------------
+/*
+let test = 'NOO'
+if (test === true){
+    console.log('Вірно')
+} else {
+    console.log('Неправильно')
+}
+*/
 
 // - Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
-//
+/*let a = -3
+if (a===0){
+    console.log('Вірно')
+} else {
+    console.log('Невірно')
+}*/
+//---------------------------------
+/*let a = 0 || true
+console.log(a)*/
+//---------------------------------
+
+/*let a = !!0 ? console.log(`ni`) : console.log(`tak`)*/
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день.
-//
+/*let rozklad = `ponedilok`;
+switch (rozklad) {
+    case "ponedilok": console.log(`jlkkjkllskklkk kkkmsl kkkm kllklk lklkl`);
+    break;
+    case `vivtorok` : console.log(`kjkkj lllkjlk lkklkl lklkl lkklk llkkkk`);
+    break;
+    case "sereda": console.log(`jlkkjkllskklkk kkkmsl kkkm kllklk lklkl`);
+        break;
+    case `thetverg` : console.log(`kjkkj lllkjlk lkklkl lklkl lkklk llkkkk`);
+        break;
+    case "piatnitsa": console.log(`jlkkjkllskklkk kkkmsl kkkm kllklk lklkl`);
+        break;
+    case `sybota` : console.log(`kjkkj lllkjlk lkklkl lklkl lkklk llkkkk`);
+        break;
+    case `nedilia` : console.log(`kjkkj lllkjlk lkklkl lklkl lkklk llkkkk`);
+    default:
+      console.log(`?????`)
+}*/
 // - Потрібно визначити Високосний рік чи ні. Високосні роки діляться націло на 4.
-//
+/*(+prompt(`Укажіть рік`) % 4 === 0) ? console.log(`високосний`) : console.log(`не високосний`)*/
 // - Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
 // Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
+    (+prompt('Яка «офіційна» назва JavaScript??') === 'ECMAScript') ? console.log('Правильно!') : console.log("Не знаєте? ECMAScript!")
 //- У нас є змінна test, якщо дорівнює true, виведіть 'Вірно', інакше виведіть 'Неправильно'.
   //  Перевірте роботу скрипта при test, що дорівнює true, false. Напишіть два варіанти скрипта - з коротким записом(тернаркою)
 /* let test = "ybbhdu"
 if (test === true){
     console.log("oK")
 } else {
-    console.log("Not")
+    console.log("Not")'
 }*/
 
 
-  //   - Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 
-//Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день.
-
-//- Потрібно визначити Високосний рік чи ні. Високосні роки діляться націло на 4.
-
-//- Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
-//Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
 
